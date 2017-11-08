454f25c2ba4030520bb010215d1a1dd6b85c57ee
akashSharma2505 <30438543+akashSharma2505@users.noreply.github.com>
2017-11-08 16:38:19 +0000

    firstname

--- fulfillment/helpers/Admin-helper.js
+++ fulfillment/helpers/Admin-helper.js
@@ -99,7 +99,7 @@ function create_cards(body, session_to_use) {
                 builder.CardImage.create(session_to_use, body[i].ProfilePic)
             ])
             .buttons([builder.CardAction.imBack(session_to_use, 'hotel for ' + body[i].UserID,'hotel for ' + body[i].UserID),
-            builder.CardAction.imBack(session_to_use, ' flight for ' + body[i].UserID,'flight for ' + body[i].UserID)])
+            builder.CardAction.imBack(session_to_use, ' flight for ' + body[i].UserID,'flight for ' + body[i].FirstName)])
         cards.push(card);
     }
     console.log(JSON.stringify(cards));
