c443f70c21bc40e621e5b4d738a291b999c650db
akashSharma2505 <30438543+akashSharma2505@users.noreply.github.com>
2017-11-09 16:22:02 +0000

    sessin as first object

--- fulfillment/helpers/greeting.js
+++ fulfillment/helpers/greeting.js
@@ -4,10 +4,10 @@ module.exports = [
     (session, args, next) => {
         if (session.userData.first_name) {
 
-            builder.Prompts.text(`Hello ${session.userData.first_name}. :)`);
+            builder.Prompts.text(session,`Hello ${session.userData.first_name}. :)`);
         } else {
             console.log(session.message.address);
-            builder.Prompts.text(`Hello there . :)`);
+            builder.Prompts.text(session,`Hello there . :)`);
         }
         builder.Prompts.choice(session, "Please choose 1 of the given options", "My flight details|My hotel details");
         session.endDialog();
