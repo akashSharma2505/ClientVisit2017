969bff262598ae0c3482fdc50d4bcc1946c9e3ec
akashSharma2505 <30438543+akashSharma2505@users.noreply.github.com>
2017-11-09 16:18:33 +0000

    greetings changes

--- fulfillment/helpers/greeting.js
+++ fulfillment/helpers/greeting.js
@@ -3,11 +3,13 @@ var builder = require("botbuilder");
 module.exports = [
     (session, args, next) => {
         if (session.userData.first_name) {
-           
-            session.endDialog(`Hello ${session.userData.first_name}. :)`);
-        } else {          
+
+            builder.Prompts.text(`Hello ${session.userData.first_name}. :)`);
+        } else {
             console.log(session.message.address);
-            session.endDialog(`Hello there . :)`);
+            builder.Prompts.text(`Hello there . :)`);
         }
+        builder.Prompts.choice(session, "Please choose 1 of the given options", "My flight details|My hotel details");
+        session.endDialog();
     }
 ];
