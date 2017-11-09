e4b18f725aafecd5a2f4572016ce000482f0c26b
akashSharma2505 <30438543+akashSharma2505@users.noreply.github.com>
2017-11-09 22:06:29 +0000

    greet

--- fulfillment/helpers/greeting.js
+++ fulfillment/helpers/greeting.js
@@ -6,13 +6,31 @@ module.exports = [
 
             session.endDialog(`Hello ${session.userData.first_name}. :)`);
             var msg = new builder.Message()
-                .attachments(
-                new builder.CardAction.imBack(session_to_use, 'hotel for ', 'hotel for ')
-                );
-            session.endDialog(msg);
+            .attachmentLayout(builder.AttachmentLayout.carousel)
+            .address(address)
+            .attachments(create_cards(body, session));
+        session.endDialog(msg);
         } else {
             console.log(session.message.address);
             session.endDialog(`Hello there . :)`);
         }
     }
 ];
+
+function create_cards(body, session_to_use) {
+    console.log(JSON.stringify(body));
+    var crew = body;
+    var cards = [];
+    for (i = 0; i < crew.length; i++) {
+
+        var item = crew[i];
+        var option = item.EmpId;
+        var card = new builder.HeroCard(session_to_use)
+            .title("Please choose one option from the given choice")
+            .buttons([builder.CardAction.imBack(session_to_use, 'Give me my flight details', 'Flight details'),
+            builder.CardAction.imBack(session_to_use, 'Give me my hotel details', 'Hotel details')])
+        cards.push(card);
+    }
+    console.log(JSON.stringify(cards));
+    return cards;
+}
