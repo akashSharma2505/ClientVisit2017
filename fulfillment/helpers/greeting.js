4142f6e434d11f1c8c978f3766a9c8d75d6b05ba
akashSharma2505 <30438543+akashSharma2505@users.noreply.github.com>
2017-11-09 16:41:53 +0000

    greeting dialog

--- fulfillment/helpers/greeting.js
+++ fulfillment/helpers/greeting.js
@@ -2,6 +2,16 @@ var builder = require("botbuilder");
 
 module.exports = [
     (session, args, next) => {
+
+        var userdetails = {
+            "Get Flight details": {
+                value: "show me my flight details",
+                
+            },
+            "Get Hotel details": {
+                value: "show me my hotel details",
+            }
+        };
         if (session.userData.first_name) {
 
             builder.Prompts.text(session,`Hello ${session.userData.first_name}. :)`);
@@ -9,7 +19,20 @@ module.exports = [
             console.log(session.message.address);
             builder.Prompts.text(session,`Hello there . :)`);
         }
-        builder.Prompts.choice(session, "Please choose 1 of the given options", "My flight details|My hotel details");
+        bot.dialog('userdetail', [
+            function (session) {
+                builder.Prompts.choice(session, "Please choose 1 of the given options",userdetails );
+            },
+            function (session, results) {
+                if (results.response) {
+                    var data = userdetails[results.response.entity];
+                    session.endDialogWithResult(`${data.value}.`); 
+                } else {
+                    session.endDialogWithResult("OK");
+                }
+            }
+        ]);
+       
         
     }
 ];
