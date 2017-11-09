a970bfd3f45cee52cd02ee7f2109b4cca0ea0992
ArjunThakurDev <arjunthakur@live.com>
2017-11-09 21:02:29 +0000

    session

--- fulfillment/helpers/greeting.js
+++ fulfillment/helpers/greeting.js
@@ -7,7 +7,7 @@ module.exports = [
             // session.endDialog(`Hello ${session.userData.first_name}. :)`);
             var msg = new builder.Message()
                 .attachments(
-                new builder.CardAction.imBack(session_to_use, 'hotel for ', 'hotel for ')
+                new builder.CardAction.imBack(session, 'hotel for ', 'hotel for ')
                 );
             session.endDialog(msg);
         } else {
