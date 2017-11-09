ed22965433a8c202d95ca06eebd1c75a1cf99818
ArjunThakurDev <arjunthakur@live.com>
2017-11-09 21:20:36 +0000

    Greet my firend

--- fulfillment/helpers/greeting.js
+++ fulfillment/helpers/greeting.js
@@ -2,15 +2,26 @@ var builder = require("botbuilder");
 
 module.exports = [
     (session, args, next) => {
+        // session.endDialog(`Hello ${session.userData.first_name}. :)`);
+        var userdetails = {
+            "Get Flight details": {
+                value: "show me my flight details",
+
+            },
+            "Get Hotel details": {
+                value: "show me my hotel details",
+            }
+        };
+
         if (session.userData.first_name) {
-
-            // session.endDialog(`Hello ${session.userData.first_name}. :)`);
-
-
-            builder.Prompts.choice(session, "Which color?", "red|green|blue");
+            builder.Prompts.text(session, `Hello ${session.userData.first_name}. :)`);
         } else {
             console.log(session.message.address);
-            session.endDialog(`Hello there . :)`);
+            builder.Prompts.text(session, `Hello there . :)`);
         }
+
+
+        builder.Prompts.choice(session, "Please choose 1 of the given options", userdetails);
+
     }
 ];
